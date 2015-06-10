3a83c51611e65b11c7b36d3f3205b5534516307a
Chaogang Wang <wangchaochina@126.com>
2015-06-10 10:43:39 +0800

    changes in hotfix

--- input.c
+++ input.c
@@ -2,7 +2,7 @@
 * @Author: wcg
 * @Date:   2015-06-10 10:10:44
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-06-10 10:26:27
+* @Last Modified time: 2015-06-10 10:42:04
 */
 
 #include <stdio.h>
@@ -18,4 +18,9 @@ string getName(){
 // This is for the change in master
 int getAll(){
     return 100;
+}
+
+// This is for the change in hotfix
+int hotfix(){
+    return 1;
 }
