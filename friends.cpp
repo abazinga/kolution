f590aac6fc0025e279257172b728738991a4c2fa
Chaogang Wang <wangchaochina@126.com>
2015-06-10 16:25:12 +0800

    first commit in kalu

---
+++ friends.cpp
@@ -0,0 +1,15 @@
+/* 
+* @Author: wcg
+* @Date:   2015-06-10 16:23:31
+* @Last Modified by:   anchen
+* @Last Modified time: 2015-06-10 16:24:24
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
