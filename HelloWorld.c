1b12dc8aa36476e59e55526db251364a70861017
Chaogang Wang <wangchaochina@126.com>
2015-06-10 09:26:27 +0800

    first commit about print hello world

---
+++ HelloWorld.c
@@ -0,0 +1,14 @@
+/* 
+* @Author: wcg
+* @Date:   2015-06-10 09:23:23
+* @Last Modified by:   anchen
+* @Last Modified time: 2015-06-10 09:25:00
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    printf("This is a good reason for %s\n", "you");
+    return 0;
+}
