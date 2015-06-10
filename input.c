8f2d9f5fa33b437ac4dfb08b07e5f0ffa91b8c93
Chaogang Wang <wangchaochina@126.com>
2015-06-10 10:37:41 +0800

    made a change in iss1

--- input.c
+++ input.c
@@ -2,7 +2,7 @@
 * @Author: wcg
 * @Date:   2015-06-10 10:10:44
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-06-10 10:26:27
+* @Last Modified time: 2015-06-10 10:37:03
 */
 
 #include <stdio.h>
@@ -18,4 +18,9 @@ string getName(){
 // This is for the change in master
 int getAll(){
     return 100;
+}
+
+//fix iss1
+int fix（）{
+    return 1;
 }
